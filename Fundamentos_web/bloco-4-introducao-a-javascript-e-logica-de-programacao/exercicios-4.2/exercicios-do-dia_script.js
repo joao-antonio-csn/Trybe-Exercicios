8c8ede12dbcc01919fe679c36d2647d64e7e254b
João Antônio Sá <joao.a.csn@gmail.com>
2022-04-07 19:05:08 -0300

    exercicio do dia - 5

--- Fundamentos_web/bloco-4-introducao-a-javascript-e-logica-de-programacao/exercicios-4.2/exercicios-do-dia_script.js
+++ Fundamentos_web/bloco-4-introducao-a-javascript-e-logica-de-programacao/exercicios-4.2/exercicios-do-dia_script.js
@@ -6,7 +6,7 @@ let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 //}
 
 // exercicio 2
-let soma = 0;
+//let soma = 0;
 
 // for (let i = 0; i < numbers.length; i+=1) {
 //   soma = soma + numbers[i];
@@ -14,14 +14,25 @@ let soma = 0;
 //  console.log(soma);
 
 // exercicio 3
-for (let i = 0; i < numbers.length; i += 1) {
-  soma = soma + numbers[i];
-}
+// for (let i = 0; i < numbers.length; i += 1) {
+//   soma = soma + numbers[i];
+// }
 
 //  console.log(soma/numbers.length);
 
-if ((soma/numbers.length) > 20) {
-  console.log("Valor maior que 20");
-} else {
-    console.log("Valor menor ou igual a 20");
+//exercicio 4
+// if ((soma/numbers.length) > 20) {
+//   console.log("Valor maior que 20");
+// } else {
+//  console.log("Valor menor ou igual a 20");
+// }
+
+//exercicio 5
+let maiorNum = numbers[0];
+for (let i = 1; i < numbers.length; i += 1) {
+  if (maiorNum < numbers[i]) {
+        maiorNum = numbers[i]; 
+  }
 }
+
+console.log(maiorNum);
