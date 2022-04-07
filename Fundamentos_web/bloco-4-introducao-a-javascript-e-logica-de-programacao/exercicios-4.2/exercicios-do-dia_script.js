9e9eb71f81e881710467d74c317d3c12ded92492
João Antônio Sá <joao.a.csn@gmail.com>
2022-04-07 17:12:37 -0300

    exercicio do dia - 4

--- Fundamentos_web/bloco-4-introducao-a-javascript-e-logica-de-programacao/exercicios-4.2/exercicios-do-dia_script.js
+++ Fundamentos_web/bloco-4-introducao-a-javascript-e-logica-de-programacao/exercicios-4.2/exercicios-do-dia_script.js
@@ -1,21 +1,27 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 
 // exercicio 1
-// for (let i = 0; i < numbers.length; i+=1) {
-//   console.log(numbers[i]);
-// }
+//for (let i = 0; i < numbers.length; i+=1) {
+//  console.log(numbers[i]);
+//}
 
 // exercicio 2
 let soma = 0;
 
 // for (let i = 0; i < numbers.length; i+=1) {
 //   soma = soma + numbers[i];
-// }
+//}
 //  console.log(soma);
 
-//exercicio 3
+// exercicio 3
 for (let i = 0; i < numbers.length; i += 1) {
   soma = soma + numbers[i];
-  }
+}
 
-console.log(soma/numbers.length);
+//  console.log(soma/numbers.length);
+
+if ((soma/numbers.length) > 20) {
+  console.log("Valor maior que 20");
+} else {
+    console.log("Valor menor ou igual a 20");
+}
