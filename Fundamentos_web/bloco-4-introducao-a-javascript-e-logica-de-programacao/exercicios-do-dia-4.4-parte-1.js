8b6f4c60ce0c1de9e1cfb83c963d766fb629ac7b
João Antônio Sá <joao.a.csn@gmail.com>
2022-04-11 18:29:52 -0300

    exercicio do dia - 4.4 - exercicio 6

--- Fundamentos_web/bloco-4-introducao-a-javascript-e-logica-de-programacao/exercicios-do-dia-4.4-parte-1.js
+++ Fundamentos_web/bloco-4-introducao-a-javascript-e-logica-de-programacao/exercicios-do-dia-4.4-parte-1.js
@@ -1,16 +1,16 @@
 //PARTE I - OBJETOS E FOR/IN
-let info = {
-    personagem: 'Margarida',
-    origem: 'Pato Donald',
-    nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
-  };
+// let info = {
+//     personagem: 'Margarida',
+//     origem: 'Pato Donald',
+//     nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
+//   };
 
   //exercicio 1
 // console.log('Bem-vinda, ' + info.personagem);
 
 //exercicio 2
 
-info['recorrente'] = 'Sim';
+// info['recorrente'] = 'Sim';
 
 // console.table(info);
 
@@ -30,16 +30,35 @@ info['recorrente'] = 'Sim';
 
 //exercicio 5
 
-let info2 = {
-    personagem: 'Tio Patinhas',
-    origem: "Christmas on Bear Mountain, Dell's Four Color Comics #178",
-    nota: 'O último MacPatinhas',
-    recorrente: 'Sim'
+// let info2 = {
+//     personagem: 'Tio Patinhas',
+//     origem: "Christmas on Bear Mountain, Dell's Four Color Comics #178",
+//     nota: 'O último MacPatinhas',
+//     recorrente: 'Sim'
+//   };
+
+//   console.log(info.personagem + ' e ' + info2.personagem);
+//   console.log(info.origem + ' e ' + info2.origem);
+//   console.log(info.nota + ' e ' + info2.nota);
+//   if(info.recorrente === 'Sim' && info2.recorrente === 'Sim') {
+//       console.log('Ambos recorrentes');
+//   }
+
+// Exercicio 6
+
+let leitor = {
+    nome: 'Julia',
+    sobrenome: 'Pessoa',
+    idade: 21,
+    livrosFavoritos: [
+      {
+        titulo: 'O Pior Dia de Todos',
+        autor: 'Daniela Kopsch',
+        editora: 'Tordesilhas',
+      },
+    ],
   };
 
-  console.log(info.personagem + ' e ' + info2.personagem);
-  console.log(info.origem + ' e ' + info2.origem);
-  console.log(info.nota + ' e ' + info2.nota);
-  if(info.recorrente === 'Sim' && info2.recorrente === 'Sim') {
-      console.log('Ambos recorrentes');
-  }
+let titulo1 = leitor.livrosFavoritos[0];
+
+console.log('O livro favorito de ' + leitor.nome + ' ' + leitor.sobrenome + ' se chama ' + titulo1['titulo']);
