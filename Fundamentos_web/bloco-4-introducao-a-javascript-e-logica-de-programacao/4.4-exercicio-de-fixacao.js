85a4c170f617f4b79f91bf87c39149566c124a1e
João Antônio Sá <joao.a.csn@gmail.com>
2022-04-11 16:35:14 -0300

    Exercicio de fixacao - 4.4 - For/in e For/of

--- Fundamentos_web/bloco-4-introducao-a-javascript-e-logica-de-programacao/4.4-exercicio-de-fixacao.js
+++ Fundamentos_web/bloco-4-introducao-a-javascript-e-logica-de-programacao/4.4-exercicio-de-fixacao.js
@@ -1,16 +1,42 @@
-let player = {
-  name: 'Marta',
-  lastName: 'Silva',
-  age: 34,
-  medals: {
-    golden: 2,
-    silver: 3, 
-  },
-  bestInTheWorld: [2006, 2007, 2008, 2009, 2010, 2018],
+//EXERCÍCIO DE FIXAÇÃO - OBJETOS
+
+// let player = {
+//   name: 'Marta',
+//   lastName: 'Silva',
+//   age: 34,
+//   medals: {
+//     golden: 2,
+//     silver: 3,
+//   },
+//   bestInTheWorld: [2006, 2007, 2008, 2009, 2010, 2018],
+// };
+
+// console.log('A jogadora ' + player.name + ' ' + player.lastName + ' tem ' + player.age + ' anos de idade');
+
+// console.log('A jogadora ' + player.name + ' ' + player.lastName + ' foi eleita a melhor do mundo por ' + player.bestInTheWorld.length + ' vezes');
+
+// console.log('A jogadora possui ' + player.medals.golden + ' medalhas de ouro e ' + player.medals.silver + ' medalhas de prata');
+
+//EXERCÍCIO DE FIXAÇÃO - FOR/IN E FOR/OF
+
+let names = {
+  person1: "João",
+  person2: "Maria",
+  person3: "Jorge",
 };
 
-console.log('A jogadora ' + player.name + ' ' + player.lastName + ' tem ' + player.age + ' anos de idade');
+for (let key in names) {
+  console.log("Olá", names[key]);
+}
 
-console.log('A jogadora ' + player.name + ' ' + player.lastName + ' foi eleita a melhor do mundo por ' + player.bestInTheWorld.length + ' vezes');
+console.log('-------------------------------------------');
 
-console.log('A jogadora possui ' + player.medals.golden + ' medalhas de ouro e ' + player.medals.silver + ' medalhas de prata');
+let car = {
+  model: "A3 Sedan",
+  manufacturer: "Audi",
+  year: 2020,
+};
+
+for (let key in car) {
+  console.log(key, car[key]);
+}
