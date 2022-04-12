9dd36c9d47362c0dadc6e5be86b6f5411bbda253
João Antônio Sá <joao.a.csn@gmail.com>
2022-04-12 11:35:39 -0300

    exercicio do dia - 4.4 - funcoes - exercicio 2

--- Fundamentos_web/bloco-4-introducao-a-javascript-e-logica-de-programacao/exercicios-do-dia-4.4-parte-2.js
+++ Fundamentos_web/bloco-4-introducao-a-javascript-e-logica-de-programacao/exercicios-do-dia-4.4-parte-2.js
@@ -7,23 +7,41 @@ Retorno esperado: true
 verificaPalindrome('desenvolvimento') ;
 Retorno esperado: false
 */ 
-let wordTest = '';
-words = ['arara', 'desenvolvimento'];
+// let wordTest = '';
+// words = ['arara', 'desenvolvimento'];
 
-for(let i = 0; i< words.length; i += 1) {
-  console.log(verificaPalindrome(words[i]));
-}
+// for(let i = 0; i< words.length; i += 1) {
+//   console.log(verificaPalindrome(words[i]));
+// }
 
-function verificaPalindrome (word) {
-  let array= [];
-  array = word.split('');
-  array = array.reverse();
-  wordTest = array.join('');
-    if(word === wordTest) {
-    return true;
-    } else {
-      return false;
+// function verificaPalindrome (word) {
+//   let array= [];
+//   array = word.split('');
+//   array = array.reverse();
+//   wordTest = array.join('');
+//     if(word === wordTest) {
+//     return true;
+//     } else {
+//       return false;
+//     }
+//   }
+
+//Exercício 2
+
+// Crie uma função que receba um array de inteiros e retorne o índice do maior valor.
+// Array de teste: [2, 3, 6, 7, 10, 1]; .
+// Valor esperado no retorno da função: 4 .
+
+arrayTest = [2, 3, 6, 7, 10, 1];
+let maiorValor = 0;
+let index = 0;
+console.log(maiorValorNoArray(arrayTest));
+function maiorValorNoArray (array) {
+  for (let i = 0; i < array.length; i+=1) {
+    if(maiorValor < array[i]) {
+      maiorValor = array[i];
+      index = i;
     }
   }
-
-  
+  return index;
+}
